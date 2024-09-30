0aed026403c9b2eeedaf27032e207dedece68711
Sid-Miram <beingsilent7777@gmail.com>
2024-09-30 18:21:04 +0530

    added update and delete

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,6 +6,8 @@ router.get("/signup", authController.signup_get);
 router.get("/login", authController.login_get);
 router.post("/login", authController.login_post);
 router.post("/signup", authController.signup_post);
+router.put("/update-password", authController.update_password);
+router.delete("/delete-user", authController.delete_user);
 
 module.exports = router;
 
