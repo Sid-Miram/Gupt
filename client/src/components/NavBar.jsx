eeef1a840e73526cd65c10ddade73864fdab0ee2
Sid-Miram <beingsilent7777@gmail.com>
2024-09-30 11:34:18 +0530

    added aboutus and docs

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,18 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import "../styles/NavBar.css"
 
-function NavBar() {
+function NavBar(){
+
   return (
     <nav>
-      <p className="Matrix">[M a t r i x]</p>
-      <div className="Clickables">
+      <p className ="Matrix">[M a t r i x] </p>
+      <div className = "Clickables">
         <Link to="/aboutus">AboutUs</Link>
-        <Link to="/documentation">Documentation</Link>
-        <Link to="/signup">SignUp</Link>
-      </div>
+        <Link to="/docs">Documentation</Link>
+        <Link to ="/signup">SignUp</Link>
+        </div>
     </nav>
-  );
+
+  )
 }
 
 export default NavBar;
-
